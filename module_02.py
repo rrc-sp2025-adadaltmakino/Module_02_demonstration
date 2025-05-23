50f326eb0bfcaceb62ca406ea758c16098c3e76f
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-05-23 10:17:06 -0500

    Experiment with built-in functions

--- module_02.py
+++ module_02.py
@@ -1,7 +1,20 @@
 """
+
 Description: Module 02 Demonstration
 Author: Amanda Dadalt Makino
 Date: 2025-05-23
 Usage: To demonstrate content from Module 02.
 
 """
+
+name = "john"
+age = "25"
+value = 3.14159
+print(f"The value is {value:2f}.")
+print(f"My name is {name} and I am {age} years old.")
+
+absolute_value = abs (-12)
+print('absolute value:' , absolute_value)
+
+# Function embedded within another function. 
+print('absolute value' , abs (-12))
