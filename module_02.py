efdfcf50dce6c682fa8885e4850ae9bec2bb1e47
Amanda Dadalt Makino <adadaltmakino@academic.rrc.ca>
2025-05-23 10:43:07 -0500

    Experiment with declaring variables of various data types and type conversion.

--- module_02.py
+++ module_02.py
@@ -5,11 +5,16 @@ Author: Amanda Dadalt Makino
 Date: 2025-05-23
 Usage: To demonstrate content from Module 02.
 
-"""
-
 name = "john"
 age = "25"
 value = 3.14159
+is_employed = True
+
+priny(type(name))
+print(type(age))
+print(type(value))
+print(type(is_employed))
+
 print(f"The value is {value:.2f}.")
 print(f"My name is {name} and I am {age} years old.")
 
@@ -18,3 +23,16 @@ print('absolute value:' , absolute_value)
 
 # Function embedded within another function. 
 print('absolute value' , abs (-12))
+
+"""
+age = 25
+current_salary = 6798.02
+age_and_salary = age + current_salary #implicit
+
+months_old = "11"
+years_old = 25
+age + float(years_old + float(months_old) / 12) #explicit
+
+print("Age as a float:" , age)
+
+print("Age as an integer:" , int(age))
